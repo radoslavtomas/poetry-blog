d8cc1651810c67ca272e3fcefe168284c4a10c8b
Radoslav Tomas <radoslav.tomas@gmail.com>
2026-06-12 15:14:40 +0100

    added deployer

---
+++ deploy.php
@@ -0,0 +1,40 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+set('repository', 'git@github.com:radoslavtomas/poetry-blog.git');
+set('keep_releases', 2);
+set('ssh_multiplexing', false);
+set('git_tty', false);
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+host('dev')
+    ->set('hostname', '139.177.183.107')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '/var/www/html/rado-poetry')
+    ->set('branch', 'main');
+
+// Hooks
+/**
+ * Main deploy task.
+ */
+desc('Deploys your project');
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    'artisan:storage:link',
+    'artisan:config:cache',
+    'artisan:route:cache',
+    'artisan:view:cache',
+    //    'artisan:event:cache',
+    //    'artisan:migrate',
+    'deploy:publish',
+]);
+
+after('deploy:failed', 'deploy:unlock');
